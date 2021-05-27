e8602193ee7d891d7d37e1ae5b2d47f5161d84ca
Cierra Smith <cierra.r.smith3@gmail.com>
2021-05-27 12:17:54 -0500

    added prompts

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//console.log("Hello World!");
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+var userInput = prompt("What is your favorite color?");
+
+if (userInput) {
+    alert(userInput + " is my favorite too.")
+}
