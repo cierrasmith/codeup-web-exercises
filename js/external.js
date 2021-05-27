782863d4119df9c55f9aa2e3fa71f80163ebfb78
Cierra Smith <cierra.r.smith3@gmail.com>
2021-05-27 14:16:47 -0500

    completed some exercises

--- js/external.js
+++ js/external.js
@@ -6,7 +6,12 @@ console.log("Hello from external JavaScript");
 alert("Welcome to my Website!");
 
 var userInput = prompt("What is your favorite color?");
+alert(userInput + " is my favorite color too.")
 
-if (userInput) {
-    alert(userInput + " is my favorite too.")
-}
+alert("The price for the movies is " + (3+5+1)* 3);
+
+alert("The payment received is " + ((400*6)+(350*10)+(380*4)));
+
+var full = confirm("Is the class full?");
+var conflict = confirm("Is there a conflict?");
+alert("You can enroll in class:" + (!full && !conflict));
