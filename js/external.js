b4bb4e048f4a72709172c70aeade5298f5fc12f9
Cierra Smith <cierra.r.smith3@gmail.com>
2021-05-27 14:50:47 -0500

    Added functions script

--- js/external.js
+++ js/external.js
@@ -15,3 +15,8 @@ alert("The payment received is " + ((400*6)+(350*10)+(380*4)));
 var full = confirm("Is the class full?");
 var conflict = confirm("Is there a conflict?");
 alert("You can enroll in class:" + (!full && !conflict));
+
+var products = confirm("Are you buying more than 2 items?");
+var premium = confirm("Are you a premium member?")
+var expired = confirm("Is the offer expired?");
+alert("Offer applied:" + (products && !expired) || premium);
