780ee75e362be2019169b50c7a1c727b43f6ae03
Cierra Smith <cierra.r.smith3@gmail.com>
2021-06-04 11:49:00 -0500

    completed some loop exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -4,15 +4,49 @@
     for (let y = 0; y <= 10; y++)
     console.log(x + " x " + y + " = " + (x * y));
 }
-showMultiplicationTable(7);*/
+showMultiplicationTable(7);
 
-//function randomNumber()
-
-for (y = 0; y <= 10; y++) {
-    let x = Math.ceil(Math.random() * 180) + 20;
+for (y = 0; y < 10; y++) {
+    let x = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
     if (x % 2 === 0) {
         console.log(x + " is even.");
     } else {
         console.log(x + " is odd.");
     }
 }
+
+
+for (let x = 0; x < 10; x++) {
+    var number=x;
+    for (let y = 0; y <= x; y++) {
+
+    }
+    console.log(number.toString() + number.toString());
+}
+
+
+
+function generatePyramid() {
+    var totalNumberofRows = 9;
+    var output = '';
+    for (var i = 1; i <= totalNumberofRows; i++) {
+        for (var j = 1; j <= i; j++) {
+            output += j + '  ';
+        }
+        console.log(output);
+        output = '';
+    }
+}
+generatePyramid();
+
+    for (let x = 1; x < 10; x++) {
+
+    var number = "";
+    number = x;
+    for (let y = 1; y < x; y++) {
+        number = number + x.toString();
+    }
+    console.log(number.toString());
+}
+*/
+
