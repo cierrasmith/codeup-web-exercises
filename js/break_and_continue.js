f8e903baed9788f3160d022688631619f9f0b717
Cierra Smith <cierra.r.smith3@gmail.com>
2021-06-07 10:05:20 -0500

    edited break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -7,7 +7,7 @@
         console.log('Here is an odd number: ' + i);
     }
 
-*/
+
 
 var oddNumber;
 
@@ -26,5 +26,27 @@ for (let i = 1; i < 50; i++) {
     if (i % 2 != 0) {
         console.log("Here is an odd number:" + i);
     }
+}
+*/
+
+let odd_Number;
+
+while (true) {
+    odd_Number = prompt("Enter an odd number between 1 and 50.");
+    if ((odd_Number != 1) && (odd_Number <= 50)) {
+        if (odd_Number % 2 == 1) {
+            break;
+         }
+    }
+}
+
+console.log("Number to skip is: " + odd_Number);
+
+for (let number = 1; number < 50; number = number + 2) {
+    if (odd_Number == number) {
+        console.log("Yikes! Skipping number: " + odd_Number);
+        continue;
+    }
+    console.log("Here is an odd number: " + number);
 }
 
--- js/while.js
+++ js/while.js
@@ -12,3 +12,21 @@ while (index < 65537) {
 
 //Ice cream cone do while exercise
 
+var totalAvailableCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    var conesBought= Math.floor(Math.random() * 5) + 1;
+    var conesLeft = totalAvailableCones - conesBought;
+
+    if(conesBought < totalAvailableCones) {
+        console.log(conesBought + " cones sold.")
+    }
+    if((conesLeft + conesBought) > totalAvailableCones ) {
+        console.log("I cannot sell you " + conesBought + " because I only have " + conesLeft);
+
+    }
+
+} while (totalAvailableCones > 100) ;
+console.log("Yay, I've sold them all!");
+
+
