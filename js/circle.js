ed0e4520b686046c01b0c3bbb2e273cdbd043b25
Cierra Smith <cierra.r.smith3@gmail.com>
2021-06-09 12:32:22 -0500

    completed math exercises

--- js/circle.js
+++ js/circle.js
@@ -6,19 +6,26 @@
         radius: 3,
 
         getArea: function () {
+            var area = (Math.PI * Math.pow(this.radius, 2));
             // TODO: complete this method
             // hint: area = pi * radius^2
+            return area; // TODO: return the proper value
 
-            return; // TODO: return the proper value
         },
+
+
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            var area = (Math.PI * Math.pow(this.radius, 2));
+            if (doRounding === true) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(area));
+            } else
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
@@ -31,9 +38,12 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
-    // log info about the circle
-    console.log("Raw circle information");
-    circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+
+    circle.radius = 5;
+
+        // log info about the circle
+        console.log("Raw circle information");
+        circle.logInfo(false);
+        console.log("Circle information rounded to the nearest whole number");
+        circle.logInfo(true);
 })();
