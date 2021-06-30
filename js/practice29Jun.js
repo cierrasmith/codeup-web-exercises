3ab1587123014f6fdf4469409feef008da3741d2
Cierra Smith <cierra.r.smith3@gmail.com>
2021-06-30 15:32:25 -0500

    practice exercises from study session

---
+++ js/practice29Jun.js
@@ -0,0 +1,34 @@
+/*var name = "MARK"
+
+var hasTheNameMark = ( name.toLowerCase() === "Mark".toLowerCase() );
+
+if (hasTheNameMark) {
+    // if condition of if statement is true, it's code body will run
+    alert("Hey I know somebody named Mark!!");
+} else {
+    alert("Hey you're not Mark!")
+}
+
+var aNumber = 78;
+
+if (aNumber % 2 === 0) {
+    // even - if a number is divisible by 2 and equal to 0
+
+    if (aNumber % 3 === 0) {
+        // aNumber is even and also divisible by 3
+
+    }
+}
+
+if (aNumber % 2 === 0 && number % 3 === 0) {
+
+    //aNumber is even and divisible by 2 and divisible by 3
+
+}
+*/
+
+function multiplyTwoNumbers(myParameter1, myParameter2) {
+    return myParameter1 * myParameter2;
+}
+console.log("call to multiply 2 numbers is : " + multiplyTwoNumbers(4, 5));
+console.log("call to multiply 2 numbers is : " + multiplyTwoNumbers(6, 7));
