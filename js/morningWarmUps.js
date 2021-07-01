f22774f0b4d5c5aadf1a4736c7e51edde87c1bf5
Cierra Smith <cierra.r.smith3@gmail.com>
2021-07-01 09:30:25 -0500

    created morning warm ups exercise

---
+++ js/morningWarmUps.js
@@ -0,0 +1,11 @@
+// TODO: Create a function named countSpaces that accepts a string and returns the total number of spaces in the string.
+
+function countSpaces(input) {
+    let newInput = input.length;
+    if (typeof newInput !== "string") {
+        return false;
+    }
+}
+console.log(countSpaces("how many spaces are in this example?"))
+
+// TODO: Add validation to function above, if the argument pass is not a string it should return false.
