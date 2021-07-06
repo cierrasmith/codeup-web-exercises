6a09bb9cc3e8da3de09e0951a19132c8b2957dd7
Cierra Smith <cierra.r.smith3@gmail.com>
2021-07-06 09:31:36 -0500

    created morning functions

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -9,3 +9,21 @@ function countSpaces(input) {
 console.log(countSpaces("how many spaces are in this example?"))
 
 // TODO: Add validation to function above, if the argument pass is not a string it should return false.
+
+// July 6th 2021
+
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+
+function capitalizeAllNames(str) {
+    newStr = str.join(' ').toUpperCase();
+}
+
+
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+function capitalizeFirstLetter(str) {
+
+}
