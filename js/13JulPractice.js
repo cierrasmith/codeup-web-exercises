9fc8ce4fb0eef50a656a66b14a233d8f3643fe26
Cierra Smith <cierra.r.smith3@gmail.com>
2021-07-13 19:18:28 -0500

    added practice exercise from study hall

---
+++ js/13JulPractice.js
@@ -0,0 +1,16 @@
+var randomTextButton = document.getElementById("randomTextButton");
+//TODO: Set variable for range input
+var randomTextContainer = document.getElementById("randomTextContainer");
+
+
+randomTextButton.addEventListener("click",function() {
+
+    var paragraphs = "";
+//TODO: Replace the number 10 with value from range input
+    for (let i = 1; i <= 10 ; i++) {
+        var paragraph = "<p>Paragraph " + i + "</p>";
+        paragraphs += paragraph;
+    }
+    randomTextContainer.innerHTML = paragraphs;
+
+})
