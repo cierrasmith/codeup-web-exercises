9c4e37977d140906fecb175a554ab8dc9aca8871
Cierra Smith <cierra.r.smith3@gmail.com>
2021-07-19 20:35:38 -0500

    updated search bar information

--- js/weather_map.js
+++ js/weather_map.js
@@ -25,12 +25,7 @@ $(document).ready(function () {
     var lat = 29.4252;
     var lon = -98.4916;
 
-    var geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    });
 
-    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
     function newWeather(coord1, coord2) {
         $.get(`https://api.openweathermap.org/data/2.5/onecall`, {
@@ -75,41 +70,6 @@ $(document).ready(function () {
     // })
 });
 
-// var day1 = data.daily[0];
-// console.log(day1);
-// $("#day1-temp").text(day1.temp.day);
-// $("#day1-description").text("Description: "+day1.weather[0].description);
-// $("#day1-humidity").text("Humidity: "+day1.humidity);
-// $("#day1-wind").text("Wind: " +day1.wind_speed);
-// $("#day1-pressure").text("Pressure: "+day1.pressure);
-//
-// var day2 = data.daily[1];
-// $("#day2-temp").text(day2.temp.day);
-// $("#day2-description").text("Description: "+day2.weather[0].description);
-// $("#day2-humidity").text("Humidity: "+day2.humidity);
-// $("#day2-wind").text("Wind: "+day2.wind_speed);
-// $("#day2-pressure").text("Pressure: "+day2.pressure);
-//
-// var day3 = data.daily[2];
-// $("#day3-temp").text(day3.temp.day);
-// $("#day3-description").text("Description: "+day3.weather[0].description);
-// $("#day3-humidity").text("Humidity: "+day3.humidity);
-// $("#day3-wind").text("Wind: "+day3.wind_speed);
-// $("#day3-pressure").text("Pressure: "+day3.pressure);
-//
-// var day4 = data.daily[3];
-// $("#day4-temp").text(day4.temp.day);
-// $("#day4-description").text("Description: "+day4.weather[0].description);
-// $("#day4-humidity").text("Humidity: "+day4.humidity);
-// $("#day4-wind").text("Wind: "+day4.wind_speed);
-// $("#day4-pressure").text("Pressure: "+day4.pressure);
-//
-// var day5 = data.daily[4];
-// $("#day5-temp").text(day5.temp.day);
-// $("#day5-description").text("Description: "+day5.weather[0].description);
-// $("#day5-humidity").text("Humidity: "+day5.humidity);
-// $("#day5-wind").text("Wind: "+day5.wind_speed);
-// $("#day5-pressure").text("Pressure: "+day5.pressure);
 
 
 
