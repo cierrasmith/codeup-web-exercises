bc36d4ac1d8b8244e1b8b1a54185913871af40fc
Cierra Smith <cierra.r.smith3@gmail.com>
2021-07-23 14:52:59 -0500

    completed exercise

--- js/promises.js
+++ js/promises.js
@@ -1,21 +1,6 @@
 "use strict";
 
-
-// function getUser() {
-//     return fetch('https://api.github.com/users/cierrasmith/events/public',
-//         {headers: {'Authorization': myGithubKey}})
-//         .then(response => response.json())
-// }
-//
-// // // later on...
-// // //
-// getUser().then( users => {
-//     users.forEach( userObj => {
-//         // do something with each username
-//         console.log(userObj.created_at);
-//     });
-// }).catch(error => console.error(error));
-
+// Handle An API Promise
 function getUser() {
 
     return fetch('https://api.github.com/users/cierrasmith/events/public',
@@ -31,3 +16,15 @@ getUser()
     }).catch(function (error) {
     console.log("ERROR!", error);
     });
+
+// Create Your Own Promise
+
+function wait(num) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Success");
+        } ,num);
+    })
+}
+wait(1000).then(() => console.log(`You'll see this after 1 second`));
+wait(3000).then(() => console.log(`You'll see this after 3 seconds`));
