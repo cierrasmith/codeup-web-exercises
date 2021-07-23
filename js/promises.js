61c1be9e01363fcdba0f10da31067a473dd85a7f
Cierra Smith <cierra.r.smith3@gmail.com>
2021-07-23 14:33:34 -0500

    completed first exercise

--- js/promises.js
+++ js/promises.js
@@ -1,3 +1,33 @@
 "use strict";
 
-fetch(url, {headers: {'Authorization': 'token' myGithubKey}})
+
+// function getUser() {
+//     return fetch('https://api.github.com/users/cierrasmith/events/public',
+//         {headers: {'Authorization': myGithubKey}})
+//         .then(response => response.json())
+// }
+//
+// // // later on...
+// // //
+// getUser().then( users => {
+//     users.forEach( userObj => {
+//         // do something with each username
+//         console.log(userObj.created_at);
+//     });
+// }).catch(error => console.error(error));
+
+function getUser() {
+
+    return fetch('https://api.github.com/users/cierrasmith/events/public',
+        {headers: {'Authorization': myGithubKey}})
+        .then(function (response) {
+            return response.json();
+        });
+}
+getUser()
+    .then(function (user) {
+        let lastCommit = user[0].created_at;
+        console.log(lastCommit);
+    }).catch(function (error) {
+    console.log("ERROR!", error);
+    });
